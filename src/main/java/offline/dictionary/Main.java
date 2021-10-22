57fec99697b58df6f0b64b660abffda6b3f31d6d
ktgun <tung.ts.work@gmail.com>
2021-10-22 12:42:17 +0700

    new function

--- src/main/java/offline/dictionary/Main.java
+++ src/main/java/offline/dictionary/Main.java
@@ -9,19 +9,27 @@ public class Main {
         Dictionary dictionary = new Dictionary();
         dictionary.ReadFileEN_VN();
 
+        dictionary.addNewWord("aothatday", "test", "just for test", "Tung");
+
+        String result = dictionary.fixLookUp("helo");
+
+        if (result != null) {
+            System.out.println(result);
+        }
+
         // tìm kiếm từ Hello
-//        Expalain ex = dictionary.LookUpEN_VN("hello");
+//        Expalain ex = dictionary.LookUpEN_VN("nome");
 //        System.out.println(ex.getDetail());
 
-        // test chức năng dự đoán từ
-        AutoComplete autoComplete = new AutoComplete();
-        // Đưa các keyWord trong Dictionary vào trieNote
-        autoComplete.addWord();
-        // dự đoán các từ với string "th" đầu vào và numCompletions là giới hạn số lượng số từ dự đoán ỉn ra theo thứ tự
-        List<String> listAutoComplete = autoComplete.predictCompletions("th", 5);
-        for (String i: listAutoComplete) {
-            System.out.println(i);
-        }
-        //
+//        // test chức năng dự đoán từ
+//        AutoComplete autoComplete = new AutoComplete();
+//        // Đưa các keyWord trong Dictionary vào trieNote
+//        autoComplete.addWord();
+//        // dự đoán các từ với string "th" đầu vào và numCompletions là giới hạn số lượng số từ dự đoán ỉn ra theo thứ tự
+//        List<String> listAutoComplete = autoComplete.predictCompletions("th", 5);
+//        for (String i: listAutoComplete) {
+//            System.out.println(i);
+//        }
+//
     }
 }
